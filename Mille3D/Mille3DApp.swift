40dd8e862de4d871ec2ee45b610a52c8463ed80d
Shakshi3104 <51497924+Shakshi3104@users.noreply.github.com>
2023-06-26 20:13:13 +0900

    Initial Commit

---
+++ Mille3D/Mille3DApp.swift
@@ -0,0 +1,17 @@
+//
+//  Mille3DApp.swift
+//  Mille3D
+//
+//  Created by MacBook Pro M1 on 2023/06/26.
+//
+
+import SwiftUI
+
+@main
+struct Mille3DApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
